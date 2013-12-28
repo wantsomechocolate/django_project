0bfab7dd5a171920d6a24b19a37a53673040dc3a
wantsomechocolate <wantsomechocolate@gmail.com>
2013-12-28 16:21:58 -0500

    Django project commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/wantsomechocolate/Code/Django/Django1.6_Tutorial/django_project/env/bin/python3.3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
