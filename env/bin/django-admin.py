7e1c221639c468a8ba52a6c9b339cbb2b226c952
wantsomechocolate <wantsomechocolate@gmail.com>
2014-01-19 20:42:20 -0500

    Round 2

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/wantsomechocolate/Code/Django/Django1.6_Tutorial/django_project/env/bin/python3.3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
